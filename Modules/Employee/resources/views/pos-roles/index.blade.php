9d06ebd0831ee74d8f475c010aaf50068b70d3eb
BadrAkbik <badrakbik1@gmail.com>
2024-10-15 14:00:56 +0300

    fix

--- Modules/Employee/resources/views/pos-roles/index.blade.php
+++ Modules/Employee/resources/views/pos-roles/index.blade.php
@@ -3,18 +3,18 @@
 @section('title', __('menuItemLang.pos_roles'))
 
 @section('content')
-    <div class="card card-flush">
+    <x-cards.card>
         <x-cards.card-header class="align-items-center py-5 gap-2 gap-md-5">
             <x-tables.table-header model="role" url="pos-role" module="employee">
                 <x-slot:export>
                     <x-tables.export-menu id="role" />
                 </x-slot:export>
-            </x-employee::tables.table-header>
-        </x-employee::card-header>
-        <x-cards.card-body class="table-responsive">
-            <x-tables.table :columns=$columns model="role" />
-        </x-employee::card-body>
-    </div>
+                </x-employee::tables.table-header>
+                </x-employee::card-header>
+                <x-cards.card-body class="table-responsive">
+                    <x-tables.table :columns=$columns model="role" />
+                    </x-employee::card-body>
+    </x-cards.card>
 @endsection
 
 @section('script')
