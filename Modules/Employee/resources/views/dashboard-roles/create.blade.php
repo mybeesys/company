93fd80e6a00f0468dae530fd88d9237258285ab3
BadrAkbik <badrakbik1@gmail.com>
2024-10-24 14:16:26 +0300

    fixed dashboard permission header

--- Modules/Employee/resources/views/dashboard-roles/create.blade.php
+++ Modules/Employee/resources/views/dashboard-roles/create.blade.php
@@ -2,11 +2,15 @@
 
 @section('title', __('menuItemLang.employees'))
 
+@section('css')
+    <style>
+    </style>
+@endsection
 @section('content')
     <form id="add_role_form" class="form d-flex flex-column flex-lg-row" method="POST" enctype="multipart/form-data"
         action="{{ route('dashboard-roles.store') }}">
         @csrf
-        <x-employee::dashboard-roles.form :modules=$modules formId="add_role_form"/>
+        <x-employee::dashboard-roles.form :modules=$modules formId="add_role_form" />
     </form>
 @endsection
 
@@ -19,6 +23,25 @@
         $(document).ready(function() {
             roleForm('add_role_form', "{{ route('dashboard-roles.create.validation') }}");
             dashboardRolePermissionsForm();
+
+            $("#dashboard-permissions-table").DataTable({
+                paging: false,
+                info: false,
+                fixedHeader: {
+                    header: true,
+                    headerOffset: 100
+                },
+                ordering: false,
+                autoWidth: false,
+            });
+            $(window).on('scroll', function() {
+                var floatingParentChild = $('.dtfh-floatingparent > div');
+                if (floatingParentChild.length) {
+                    floatingParentChild.css('padding-right', '0');
+                    $('.dtfh-floatingparent').addClass('rounded-start rounded-end');
+                }
+            });
+
         });
     </script>
 @endsection
