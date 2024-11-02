74015703bc49ed6de580904a5ea6d461aacbf346
dh_hiba_dimashky <hd@digital-horizon.net>
2024-11-03 00:28:29 +0200

    fix bug

--- resources/components/discount/TreeTableDiscount.jsx
+++ resources/components/discount/TreeTableDiscount.jsx
@@ -327,7 +327,7 @@ const TreeTableDiscount = ({ urlList, rootElement, translations, discountTypes, 
 
             <div class="card-header border-0 pt-5">
                 <h3 class="card-title align-items-start flex-column">
-                    <span class="card-label fw-bold fs-3 mb-1">{translations.serviceFees}</span>
+                    <span class="card-label fw-bold fs-3 mb-1">{translations.discount}</span>
                 </h3>
                 <div class="card-toolbar">
                     <div class="d-flex align-items-center gap-2 gap-lg-3">
