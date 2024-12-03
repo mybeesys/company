2c1db824b22151424534cd03cf9e2934fc1d59b3
dh_hiba_dimashky <hd@digital-horizon.net>
2024-12-03 19:59:32 +0200

    merge

--- Modules/Product/Models/Ingredient.php
+++ Modules/Product/Models/Ingredient.php
@@ -65,5 +65,10 @@ class Ingredient extends Model
         return $this->belongsTo(ProductInventory::class, 'id', 'ingredient_id');
     }
 
+    public function unitTransfers()
+    {
+        return $this->hasMany(UnitTransfer::class, 'ingredient_id', 'id');
+    }
+
 }
 ?>
