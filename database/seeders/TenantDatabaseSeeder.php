4bdafbf39ee2da78bf94bc446c53d33d04036af5
mhd_amen042 <miladalmani03@gmail.com>
2024-12-05 16:44:21 +0300

    change configration

--- database/seeders/TenantDatabaseSeeder.php
+++ database/seeders/TenantDatabaseSeeder.php
@@ -11,7 +11,7 @@ use Modules\Accounting\database\seeders\AccountingDatabaseSeeder;
 use Modules\Establishment\database\seeders\EstablishmentDatabaseSeeder;
 use Modules\UserManagement\database\seeders\UserManagementDatabaseSeeder;
 use Modules\Employee\database\seeders\EmployeeDatabaseSeeder;
-use Modules\General\Database\Seeders\GeneralDatabaseSeeder;
+use Modules\General\Database\seeders\GeneralDatabaseSeeder;
 use Modules\Product\database\seeders\DiningTypeSeeder;
 use Modules\Product\database\seeders\ProductDatabaseSeeder;
 
