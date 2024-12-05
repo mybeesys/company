83860d43dda4c7ced555f55e953dd5fd9791ae4c
BadrAkbik <badrakbik1@gmail.com>
2024-12-05 13:30:14 +0300

    establishment id

---
+++ Modules/Employee/Http/Controllers/Api/TimeSheetRuleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Employee\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class TimeSheetRuleController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
