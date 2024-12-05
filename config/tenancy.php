c15ae29aabec24f1d0c5476435b6c1398bf3038a
mhd_amen042 <miladalmani03@gmail.com>
2024-12-05 16:31:12 +0300

    fix configration

--- config/tenancy.php
+++ config/tenancy.php
@@ -192,9 +192,9 @@ return [
             'Modules/Product/database/migrations/tenant',
             'Modules/Accounting/database/migrations/tenant',
             'Modules/Inventory/database/migrations/tenant',
-            'Modules/clientsandsuppliers/database/migrations/tenant',
-            'Modules/sales/database/migrations/tenant',
-            'Modules/general/database/migrations/tenant',
+            'Modules/ClientsAndSuppliers/database/migrations/tenant',
+            'Modules/Sales/database/migrations/tenant',
+            'Modules/General/database/migrations/tenant',
         ],
 
         '--realpath' => true,
