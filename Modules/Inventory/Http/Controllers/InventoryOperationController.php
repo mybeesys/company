85a3bad3517c3cc1a5ab7b99d32e1ba2e613e7c4
dh_hiba_dimashky <hd@digital-horizon.net>
2025-01-12 09:45:20 +0200

    product & inventory

--- Modules/Inventory/Http/Controllers/InventoryOperationController.php
+++ Modules/Inventory/Http/Controllers/InventoryOperationController.php
@@ -94,6 +94,7 @@ class InventoryOperationController extends Controller
             $prodTotal = array_filter($prodTotals->toArray(), function($value)use($prod) {
                 return $prod->product_id == $value["product_id"]; // Keep only even numbers
             });
+            dd($prodTotal);
             $prodTotal = reset($prodTotal);
             $totalQty = isset($times) && $times!=null ? $prod->qty * $times : $prod->qty;
             if($prodTotal["qty"] == null || $prodTotal["qty"] < $totalQty){
