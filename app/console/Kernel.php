3abdb7c2f498fc3027203eb486c3564cccbe93bf
hibadimashky93 <hibadimashky93@gmail.com>
2025-01-20 00:13:45 +0300

    reminder

--- app/console/Kernel.php
+++ app/console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inventory:ten-minute-reminder')->everyTenSeconds()
+        $schedule->command('inventory:ten-minute-reminder')->everyMinute()
         ->sendOutputTo(storage_path('logs/inventory_schedule_output.log'));
     }
 
