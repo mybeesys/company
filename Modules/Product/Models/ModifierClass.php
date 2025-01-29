ef4d716036e4dd93bad36778519e6d7f54683dec
hibadimashky93 <hibadimashky93@gmail.com>
2025-01-29 23:02:33 +0300

    .

---
+++ Modules/Product/Models/ModifierClass.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Modules\Product\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Modules\Product\Database\Factories\ModifierclassFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ModifierClass extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $table = 'product_modifierclasses';
+        
+    public $timestamps = true;
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'name_ar',
+        'name_en',
+        'order',
+        'active'
+    ];
+
+    public function getFillable(){
+        return $this->fillable;
+    }
+
+    public $type = 'modifierClass';
+    public $childType = 'modifier';
+    public $childKey = 'class_id';
+
+    public function children()
+    {
+        return $this->hasMany(Modifier::class, 'class_id', 'id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(ProductModifier::class, 'modifier_id', 'id');
+    }
+}
