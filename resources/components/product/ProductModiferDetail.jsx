c612711cf98d551da22d922c88d7410613d7e43b
mhd_amen042 <miladalmani03@gmail.com>
2025-02-19 14:27:24 +0300

    test Style

--- resources/components/product/ProductModiferDetail.jsx
+++ resources/components/product/ProductModiferDetail.jsx
@@ -45,60 +45,63 @@ const ProductModiferDetail = ({ translations, modifierId, title, productModifier
 
     return (
         <section class="product spad">
-            <div class="container mt-5">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="trending__product">
-                            <div class="row border-bottom">
-                                <div class="col-lg-8 col-md-8 col-sm-8">
-                                    <div class="section-title">
-                                        <h4>{title}</h4>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4 col-md-4 col-sm-4">
-                                    <div class="btn__all">
-                                        <a href="#" class="primary-btn" onClick={handleSelectAll}>Select All<span class="arrow_right"></span></a>
-                                    </div>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="trending__product">
+                        <div class="row border-bottom">
+                            <div class="col-lg-8 col-md-8 col-sm-8">
+                                <div class="section-title">
+                                    <h4>{title}</h4>
                                 </div>
                             </div>
-                            <div class="container">
+                            <div class="col-lg-4 col-md-4 col-sm-4">
+                                <div class="btn__all">
+                                    <a href="#" class="primary-btn" onClick={handleSelectAll}>Select All<span class="arrow_right"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container">
                             <div class="row border-bottom border-dark">
                                 <form onSubmit={(event) => clickSubmit(event)}>
                                     <div class="form-group" style={{ paddingtop: '5px' }}>
                                         <div class="row">
                                             <div class="col-4">
-                                                <div class="d-flex  align-items-center ">
-                                                    <label class="fs-6 fw-semibold mb-2 me-3 "
-                                                        style={{width: "50px !important"}}>{translations.active}</label>
+                                                <div class="d-flex align-items-center">
+                                                    <label class="fs-6 fw-semibold mb-2 me-3" style={{ width: "50px !important" }}>
+                                                        {translations.active}
+                                                    </label>
                                                     <div class="form-check">
-                                                        <input type="checkbox" style={{border: "1px solid #9f9f9f"}}
+                                                        <input type="checkbox" style={{ border: "1px solid #9f9f9f" }}
                                                             class="form-check-input my-2"
-                                                            id="active" checked={modifierClass.active ==0 ? false : true}
-                                                            onChange={(e) => handleChange('active', e.target.checked ? 1 : 0)}/>
+                                                            id="active" checked={modifierClass.active == 0 ? false : true}
+                                                            onChange={(e) => handleChange('active', e.target.checked ? 1 : 0)} />
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="col-4">
-                                                <div class="d-flex  align-items-center ">
-                                                    <label class="fs-6 fw-semibold mb-2 me-3 "
-                                                        style={{width: "50px !important"}}>{translations.required}</label>
+                                                <div class="d-flex align-items-center">
+                                                    <label class="fs-6 fw-semibold mb-2 me-3" style={{ width: "50px !important" }}>
+                                                        {translations.required}
+                                                    </label>
                                                     <div class="form-check">
-                                                        <input type="checkbox" style={{border: "1px solid #9f9f9f"}}
+                                                        <input type="checkbox" style={{ border: "1px solid #9f9f9f" }}
                                                             class="form-check-input my-2"
-                                                            id="required" checked={modifierClass.required ==0 ? false : true}
-                                                            onChange={(e) => handleChange('required', e.target.checked ? 1 : 0)}/>
+                                                            id="required" checked={modifierClass.required == 0 ? false : true}
+                                                            onChange={(e) => handleChange('required', e.target.checked ? 1 : 0)} />
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="col-4">
-                                            <div class="d-flex  align-items-center ">
-                                                    <label class="fs-6 fw-semibold mb-2 me-3 "
-                                                        style={{width: "50px !important"}}>{translations.defaultModifier}</label>
+                                                <div class="d-flex align-items-center">
+                                                    <label class="fs-6 fw-semibold mb-2 me-3" style={{ width: "50px !important" }}>
+                                                        {translations.defaultModifier}
+                                                    </label>
                                                     <div class="form-check">
-                                                        <input type="checkbox" style={{border: "1px solid #9f9f9f"}}
+                                                        <input type="checkbox" style={{ border: "1px solid #9f9f9f" }}
                                                             class="form-check-input my-2"
-                                                            id="required" checked={modifierClass.default ==0 ? false : true}
-                                                            onChange={(e) => handleChange('default', e.target.checked ? 1 : 0)}/>
+                                                            id="default" checked={modifierClass.default == 0 ? false : true}
+                                                            onChange={(e) => handleChange('default', e.target.checked ? 1 : 0)} />
                                                     </div>
                                                 </div>
                                             </div>
@@ -110,37 +113,36 @@ const ProductModiferDetail = ({ translations, modifierId, title, productModifier
                                                 <label for="min_modifiers" class="col-form-label">{translations.min}</label>
                                                 <input type="number" id="min_modifiers" min="0" class="form-control form-control-solid custom-height" value={modifierClass.min_modifiers}
                                                     onChange={(e) => handleChange('min_modifiers', e.target.value)}
-                                                    required></input>
+                                                    required />
                                             </div>
                                             <div class="col-4">
                                                 <label for="max_modifiers" class="col-form-label">{translations.max}</label>
                                                 <input type="number" id="max_modifiers" min="0" class="form-control form-control-solid custom-height" value={modifierClass.max_modifiers}
-                                                    onChange={(e) => handleChange('max_modifiers', e.target.value)}
-                                                ></input>
+                                                    onChange={(e) => handleChange('max_modifiers', e.target.value)} />
                                             </div>
                                             <div class="col-4">
                                                 <label for="display_order" class="col-form-label">{`${translations.display} ${translations.order}`}</label>
                                                 <input type="number" id="display_order" class="form-control form-control-solid custom-height" value={modifierClass.display_order}
                                                     onChange={(e) => handleChange('display_order', e.target.value)}
-                                                    required></input>
+                                                    required />
                                             </div>
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <div class="row">
                                             <div class="col-4">
-                                            <label for="button_display" class="col-form-label">{`${translations.button} ${translations.display}`}</label>
-                                            <Select
-                                                id="button_display"
-                                                isMulti={false}
-                                                options={displayOptions}
-                                                closeMenuOnSelect={true}
-                                                components={animatedComponents}
-                                                value={displayOptions[modifierClass.button_display]}
-                                                onChange={val => handleChange('button_display', val.value, val)}
-                                                menuPortalTarget={document.body}
-                                                styles={{ menuPortal: base => ({ ...base, zIndex: 100000 }) }}
-                                            />
+                                                <label for="button_display" class="col-form-label">{`${translations.button} ${translations.display}`}</label>
+                                                <Select
+                                                    id="button_display"
+                                                    isMulti={false}
+                                                    options={displayOptions}
+                                                    closeMenuOnSelect={true}
+                                                    components={animatedComponents}
+                                                    value={displayOptions[modifierClass.button_display]}
+                                                    onChange={val => handleChange('button_display', val.value, val)}
+                                                    menuPortalTarget={document.body}
+                                                    styles={{ menuPortal: base => ({ ...base, zIndex: 100000 }) }}
+                                                />
                                             </div>
                                             <div class="col-4">
                                                 <label for="modifier_display" class="col-form-label">{`${translations.modifier} ${translations.display}`}</label>
@@ -161,11 +163,12 @@ const ProductModiferDetail = ({ translations, modifierId, title, productModifier
                                 </form>
                             </div>
                         </div>
-                        </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
+
     );
 };
 
