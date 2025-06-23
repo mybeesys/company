47275516b8ce973c160f0e6d6ca6f1e9bee92e37
mhd_amen042 <miladalmani03@gmail.com>
2025-06-23 18:24:32 +0300

    ..

--- Modules/Product/Models/Transformers/ProductResource.php
+++ Modules/Product/Models/Transformers/ProductResource.php
@@ -4,6 +4,7 @@ namespace Modules\Product\Models\Transformers;
 
 use App\Helpers\TaxHelper;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\General\Transformers\TaxResource;
 
 class ProductResource extends JsonResource
 {
@@ -38,8 +39,7 @@ class ProductResource extends JsonResource
             $tax["name"] = $this->tax["name"];
             $tax["value"] = TaxHelper::getTax($this->price, $this->tax->amount);
             $tax["is_tax_group"] = $this->tax["is_tax_group"];
-            $tax["sub_taxes"] = $this->sub_taxes;
-
+            $tax["sub_taxes"] = TaxResource::collection($this->sub_taxes);
         }
 
         $extraData = ['withProduct' => 'N', 'parent_id' => $this->id];
