8fd5966071b3469d9ed3c30578fcfad65c2cee22
mhd_amen042 <miladalmani03@gmail.com>
2025-06-29 11:39:29 +0300

    ..

--- Modules/Employee/Http/Controllers/Api/EmployeeController.php
+++ Modules/Employee/Http/Controllers/Api/EmployeeController.php
@@ -11,13 +11,17 @@ class EmployeeController extends Controller
 {
     public function index(Request $request)
     {
-        $request->validate([
-            'establishment_id' => ['nullable', 'exists:est_establishments,id']
-        ]);
-        $employees = Employee::with(['posRoles', 'defaultEstablishment', 'wage', 'allowances', 'deductions'])
-            ->when($request->query('establishment_id'), function ($query) use ($request) {
-                $query->whereHas('defaultEstablishment', fn($query) => $query->where('id', $request->query('establishment_id')));
-            })->get();
-        return new EmployeeCollection($employees);
+        try {
+            $request->validate([
+                'establishment_id' => ['nullable', 'exists:est_establishments,id']
+            ]);
+            $employees = Employee::with(['posRoles', 'defaultEstablishment', 'wage', 'allowances', 'deductions'])
+                ->when($request->query('establishment_id'), function ($query) use ($request) {
+                    $query->whereHas('defaultEstablishment', fn($query) => $query->where('id', $request->query('establishment_id')));
+                })->get();
+            return new EmployeeCollection($employees);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e], 500);
+        }
     }
 }
