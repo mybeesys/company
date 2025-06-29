1d3818749245d514b1d2bac8a2f5996ee3b66653
mhd_amen042 <miladalmani03@gmail.com>
2025-06-29 11:49:54 +0300

    ..

--- Modules/Employee/routes/Api/employee.php
+++ Modules/Employee/routes/Api/employee.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Modules\Employee\Http\Controllers\Api\EmployeeController;
 
 
-Route::controller(EmployeeController::class)->middleware(['auth-central'])->group(function(){
+// Route::controller(EmployeeController::class)->middleware(['auth-central'])->group(function(){
+//     Route::get('/employees', 'index');
+// });
+Route::controller(EmployeeController::class)->group(function(){
     Route::get('/employees', 'index');
 });
