d932a2285d224513c1ca02da4ae5d487ff9f44d2
Noura-Altahhan <nouraaltahan1997@gmail.com>
2025-08-23 15:46:32 +0300

    Update dashboard.blade.php

--- Modules/Inventory/resources/views/dashboard/dashboard.blade.php
+++ Modules/Inventory/resources/views/dashboard/dashboard.blade.php
@@ -4,9 +4,7 @@
 @section('css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/apexcharts@3.35.0/dist/apexcharts.min.js" rel="stylesheet">
 <style>
     :root {
         --primary-color: #4361ee;
