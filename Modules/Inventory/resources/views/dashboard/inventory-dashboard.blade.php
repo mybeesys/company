89f791095833af70784ca01044d9859cab7b7c52
Noura-Altahhan <nouraaltahan1997@gmail.com>
2025-08-23 14:04:18 +0300

    Update inventory-dashboard.blade.php

--- Modules/Inventory/resources/views/dashboard/inventory-dashboard.blade.php
+++ Modules/Inventory/resources/views/dashboard/inventory-dashboard.blade.php
@@ -279,7 +279,7 @@
                     <i class="fas fa-warehouse"></i>
                 </div>
                 <div class="content">
-                    <h6 class="title">@lang('Inventory::dashboard.warehouses_count')</h6>
+                    <h6 class="title">@lang('product::dashboard.total_products')</h6>
                     <h3 class="value text-primary">{{ $warehousesCount ?? 0 }}</h3>
                 </div>
             </div>
