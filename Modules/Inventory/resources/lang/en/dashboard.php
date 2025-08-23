d3ff0b9d67eea92c876319329f6eac6773f9c6ed
Noura-Altahhan <nouraaltahan1997@gmail.com>
2025-08-23 13:26:27 +0300

    update

---
+++ Modules/Inventory/resources/lang/en/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'warehouse_dashboard' => 'Warehouse Dashboard',
+    'warehouses_count' => 'Total Warehouses',
+    'transfers_count' => 'Total Transfers',
+    'preparations_count' => 'Total Preparations',
+    'waste_count' => 'Total Waste',
+    'inventory_overview' => 'Warehouse Inventory Overview',
+    'no_products' => 'No products',
+    'highest_quantity' => 'Highest Quantity',
+    'lowest_quantity' => 'Lowest Quantity',
+    'quantity' => 'Quantity',
+    'no_warehouses' => 'No warehouses available to display inventory.',
+    'view_products' => 'View Products',
+    'damaged_products' => 'Damaged Products',
+    'transfer_product' => 'Transfer Product',
+    'prepare_recipe' => 'Prepare Recipe',
+];
