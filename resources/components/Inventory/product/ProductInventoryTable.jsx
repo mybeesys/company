20259977524e3c8af7bbf259c6a9a8b304cbadf9
Noura-Altahhan <nouraaltahan1997@gmail.com>
2025-08-26 06:31:19 +0300

    Update ProductInventoryTable.jsx

--- resources/components/Inventory/product/ProductInventoryTable.jsx
+++ resources/components/Inventory/product/ProductInventoryTable.jsx
@@ -12,8 +12,8 @@ const ProductInventoryTable = ({ dir, translations, p_type }) => {
         JSON.parse(rootElement.getAttribute("list-url"))
     );
     const [searchBy, setSearchBy] = useState({
-        label: translations.product,
-        value: 1,
+        label: translations.establishment,
+        value: 0,
     });
     const [searchText, setSearchText] = useState();
     const [isTransactionModalVisible, setIsTransactionModalVisible] =
@@ -99,6 +99,7 @@ const ProductInventoryTable = ({ dir, translations, p_type }) => {
                             id="search_id"
                             isMulti={false}
                             options={[
+                                { label: translations.establishment, value: 0 },
                                 { label: translations.product, value: 1 },
                             ]}
                             closeMenuOnSelect={true}
