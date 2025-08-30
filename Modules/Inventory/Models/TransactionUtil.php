0643d1a9a4f03a502f9875f9858f61e4cf908d9b
Noura-Altahhan <nouraaltahan1997@gmail.com>
2025-08-31 00:57:06 +0300

    Update TransactionUtil.php

--- Modules/Inventory/Models/TransactionUtil.php
+++ Modules/Inventory/Models/TransactionUtil.php
@@ -256,6 +256,7 @@ class TransactionUtil
                     "name_en" => $product->name_en,
                     "qty" => 0
                 ];
+                continue;
             }
             $hasSubUnit = UnitTransfer::where('id', $prod->unit_id)
                 ->first();
