b871cb013a6f2d11c90bef392baa3fac81055473
Noura-Altahhan <nouraaltahan1997@gmail.com>
2025-09-06 18:33:57 +0300

    Update ProductInventoryController.php

--- Modules/Inventory/Http/Controllers/ProductInventoryController.php
+++ Modules/Inventory/Http/Controllers/ProductInventoryController.php
@@ -301,14 +301,13 @@ class ProductInventoryController extends Controller
                     ->get();
             }
         } else {
-            $establishments = Establishment::select('est_establishments.id', 'est_establishments.name')
-                ->join('product_inventories', 'est_establishments.id', '=', 'product_inventories.establishment_id')
-                ->join('product_products', 'product_inventories.product_id', '=', 'product_products.id')
+            $establishments = DB::table('product_products')
+                ->select('product_products.id', 'product_products.name_ar', 'product_products.name_en')
+                ->join('product_inventories', 'product_products.id', '=', 'product_inventories.product_id')
                 ->where(function ($query) use ($key) {
                     $query->where('product_products.name_ar', 'LIKE', "%{$key}%")
                         ->orWhere('product_products.name_en', 'LIKE', "%{$key}%");
                 })
-                ->whereNull('est_establishments.deleted_at')
                 ->distinct()
                 ->get();
         }
