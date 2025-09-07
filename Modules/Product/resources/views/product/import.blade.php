7ca9c5111637311cec3dac9a323d0b53cb4c21e4
Noura-Altahhan <nouraaltahan1997@gmail.com>
2025-09-07 15:54:21 +0300

    update

--- Modules/Product/resources/views/product/import.blade.php
+++ Modules/Product/resources/views/product/import.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    @viteReactRefresh
-    @vite('resources/components/App.jsx')
-   						
-      <div id="root" type="importProduct"
-      data-type = "products"
-	  category-url="{{ json_encode(route('category.index'))}}"
-    template-url="{{'/assets/media/svg/files/products.xlsx'}}"
-	  dir = "{{ app()->getLocale() == 'en'? 'ltr' : 'rtl'}}"></div>
+@viteReactRefresh
+@vite('resources/components/App.jsx')
+
+<div id="root" type="importProduct"
+  data-type="products"
+  category-url="{{ json_encode(route('category.index'))}}"
+  template-url="{{'/assets/media/svg/files/product.xlsx'}}"
+  dir="{{ app()->getLocale() == 'en'? 'ltr' : 'rtl'}}"></div>
 
 @endsection
