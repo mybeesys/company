00cbf4283636d8a85b9b0b7f714547bcf65dd8ea
Noura-Altahhan <nouraaltahan1997@gmail.com>
2025-10-04 15:42:56 +0300

    Update SellController.php

--- Modules/Sales/Http/Controllers/SellController.php
+++ Modules/Sales/Http/Controllers/SellController.php
@@ -351,29 +351,27 @@ class SellController extends Controller
 
             //$is_recipe_yield = Product::find($product->products_id)->recipe_yield;
             //if ($is_recipe_yield) {
-            $recipeProducts = RecipeProduct::with('products')->where('product_id', $product->products_id)->get();
-
-            if ($recipeProducts->isNotEmpty()) {
-                foreach ($recipeProducts as $recipeProduct) {
-                    foreach ($recipeProduct->products as $ingredient) {
-                        // $price_with_tax = $ingredient->type == 'ingredint' ? $ingredient->orderPriceWithTax : $ingredient->price_with_tax;
-
-                        TransactionSellLine::create([
-                            'transaction_id' => $transaction->id,
-                            'product_id' => $ingredient->id,
-                            'qyt' => $product->qty * $recipeProduct->quantity,
-                            'unit_id' => $recipeProduct->unit_transfer_id,
-                            'unit_price_before_discount' => $ingredient->price ?? 0,
-                            'unit_price' => $ingredient->price ?? 0,
-                            'discount_type' => 'fixd',
-                            'discount_amount' => 0,
-                            'unit_price_inc_tax' => 0,
-                            'tax_id' => $ingredient->tax_id,
-                            'tax_value' => 0,
-                            'total_before_vat' => $ingredient->price ?? 0,
-                            'is_show' => 0,
-                        ]);
-                    }
+            $recipeProduct = RecipeProduct::with('products')->where('product_id', $product->products_id)->first();
+            if ($recipeProduct) {
+                foreach ($products as $product) {
+                    $discount_type = $product->discount ? $product->discount_type : null;
+                    $price_with_tax = 0;
+                    $price_with_tax = $recipeProduct->products->type == 'ingredint' ? $recipeProduct->products->orderPriceWithTax : $recipeProduct->products->price_with_tax;
+                    TransactionSellLine::create([
+                        'transaction_id' => $transaction->id,
+                        'product_id' => $recipeProduct->products->id,
+                        'qyt' => $product->qty * $recipeProduct->quantity,
+                        'unit_id' => $recipeProduct->unit_transfer_id,
+                        'unit_price_before_discount' => $recipeProduct->products->price ?? 0,
+                        'unit_price' =>  $recipeProduct->products->price ?? 0,
+                        'discount_type' => 'fixd',
+                        'discount_amount' => 0,
+                        'unit_price_inc_tax' =>  $price_with_tax,
+                        'tax_id' => $recipeProduct->products->tax_id,
+                        'tax_value' => $price_with_tax - ($recipeProduct->products->price ?? 0),
+                        'total_before_vat' => $recipeProduct->products->price ?? 0,
+                        'is_show' => 0,
+                    ]);
                 }
             }
         }
