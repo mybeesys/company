b9ab1d748d9bb8d5080dee0ce6a9de307a62f63f
MohammadAmen <mohammadamen042@gmail.com>
2026-02-10 16:15:47 +0300

    ty

--- Modules/Reservation/Http/Controllers/Api/OrderController.php
+++ Modules/Reservation/Http/Controllers/Api/OrderController.php
@@ -415,7 +415,7 @@ class OrderController extends Controller
     /**
      * Show the specified resource.
      */
-    public function cancelOrder(Reqest $id)
+    public function cancelOrder(Reqest $request)
     {
         try{
 
