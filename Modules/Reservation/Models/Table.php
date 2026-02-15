e63696eb718934ffab76b6d861e70eeb4a673fe6
MohammadAmen <mohammadamen042@gmail.com>
2026-02-15 12:48:20 +0300

    .

--- Modules/Reservation/Models/Table.php
+++ Modules/Reservation/Models/Table.php
@@ -35,7 +35,7 @@ class Table extends Model
     public function activeOrder()
     {
         return $this->hasOne(TableOrders::class, 'table_id')
-            ->whereIn('order_status', ['active'])
+            ->whereIn('order_status', ['inpreparation'])
             ->latest();
     }
 
