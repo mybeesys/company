210b447d4f9a24b8ddaa8f5ad6649609baa7839e
mhd_amen042 <miladalmani03@gmail.com>
2026-03-08 13:34:01 +0300

    ..

--- Modules/General/Utils/TransactionUtils.php
+++ Modules/General/Utils/TransactionUtils.php
@@ -106,7 +106,8 @@ class TransactionUtils
         }
 
         $prefix_type = $transaction->type == 'purchase' ? 'purchase_payment' : 'sell_payment';
-        $date = Carbon::parse($request->payment_on);
+        $date = $request->filled('payment_on') ? Carbon::parse($request->payment_on) : now();
+
         $payment_on = $date->format('Y-m-d H:i:s');
         $due_account_id = '';
         $cash_account_id = '';
