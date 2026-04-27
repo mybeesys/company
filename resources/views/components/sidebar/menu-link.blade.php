d997882a7c2ae91f0c35fda59b686b231505a1d9
mhd_amen042 <miladalmani03@gmail.com>
2026-04-27 14:20:05 +0300

    ..

--- resources/views/components/sidebar/menu-link.blade.php
+++ resources/views/components/sidebar/menu-link.blade.php
@@ -1,4 +1,12 @@
 @props(['name', 'icon' => null, 'subMenuCount' => null])
+@php
+    $menuLabelKey = 'menuItemLang.' . $name;
+    $menuLabelFallbackKey = 'lang.' . $name;
+    $menuLabel = __($menuLabelKey);
+    if ($menuLabel === $menuLabelKey && \Illuminate\Support\Facades\Lang::has($menuLabelFallbackKey)) {
+        $menuLabel = __($menuLabelFallbackKey);
+    }
+@endphp
 <span class="menu-link">
     @if (!$icon)
         <span class="menu-bullet">
@@ -9,7 +17,7 @@
             <i class='{{ $icon }}'></i>
         </span>
     @endif
-    <span class="menu-title fs-6">{{ __('menuItemLang.' . $name) }}</span>
+    <span class="menu-title fs-6">{{ $menuLabel }}</span>
     @if ($subMenuCount)
         <span class="menu-arrow"></span>
     @endif
