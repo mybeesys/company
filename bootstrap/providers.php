65371571d2abf3ec41b03b8a82628cad73010981
mhd_amen042 <miladalmani03@gmail.com>
2026-04-27 14:29:07 +0300

    ..

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\TenancyServiceProvider::class,
-    Illuminate\Translation\TranslationServiceProvider::class,
     Modules\Product\Providers\RouteServiceProvider::class,
     Modules\Inventory\Providers\RouteServiceProvider::class,
     Modules\Inventory\Providers\InventoryServiceProvider::class,
